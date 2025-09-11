fabcabc7b3ffd024c9599455cf73d0e359cac6a7
fanslee <86940781+fanslee@users.noreply.github.com>
2025-09-11 21:10:29 +0800

    Create bg.js

---
+++ bg.js
@@ -0,0 +1,27 @@
+<script type="text/javascript">
+    //_1920x1080.jpg _UH*D.jpg
+    var bodyBgs = [];
+    bodyBgs[0] = "//cn.bing.com/th?id=OHR.SpringPoint_ZH-CN6445792697_1920x1080.jpg";
+    bodyBgs[1] = "//cn.bing.com/th?id=OHR.BrightEye_ZH-CN6196887876_1920x1080.jpg";
+    bodyBgs[2] = "//cn.bing.com/th?id=OHR.Atoll_ZH-CN9469093805_1920x1080.jpg";
+    bodyBgs[3] = "//cn.bing.com/th?id=OHR.LakePowellAerial_ZH-CN1427611965_1920x1080.jpg";
+    bodyBgs[4] = "//cn.bing.com/th?id=OHR.BenagilCave_ZH-CN0480408879_1920x1080.jpg";
+    bodyBgs[5] = "//cn.bing.com/th?id=OHR.MauiWhale_ZH-CN6664793962_1920x1080.jpg";
+    bodyBgs[6] = "//cn.bing.com/th?id=OHR.BlueBahamas_ZH-CN2083290847_1920x1080.jpg";
+    bodyBgs[7] = "//cn.bing.com/th?id=OHR.MorroJable_ZH-CN7382027688_1920x1080.jpg";
+    bodyBgs[8] = "//cn.bing.com/th?id=OHR.QiXiFestival2022_ZH-CN2628111266_1920x1080.jpg";
+    bodyBgs[9] = "//cn.bing.com/th?id=OHR.GentooGrievances_ZH-CN2875292726_1920x1080.jpg";
+    bodyBgs[10] = "//cn.bing.com/th?id=OHR.AntarcticaDay_ZH-CN5719164468_1920x1080.jpg";
+    bodyBgs[11] = "//cn.bing.com/th?id=OHR.CelebratingSurfing_ZH-CN9747833506_1920x1080.jpg";
+    // bodyBgs[0] = "//cn.bing.com/th?id=OHR.TheChaps_ZH-CN5966508162_1920x1080.jpg"; 热
+    // bodyBgs[7] = "//cn.bing.com/th?id=OHR.SunriseCastle_ZH-CN6235928386_1920x1080.jpg"; 热
+    // bodyBgs[12] = "//cn.bing.com/th?id=OHR.NationalDay2022_ZH-CN3861603311_1920x1080.jpg"; 热
+    // bodyBgs[3] = "//cn.bing.com/th?id=OHR.ThreeWildebeest_ZH-CN0175563521_1920x1080.jpg"; 热
+    bodyBgs[12] = "https://cn.bing.com/th?id=OHR.SumatranRhino_ZH-CN4529744910_1920x1080.jpg";
+    bodyBgs[13] = "https://cn.bing.com/th?id=OHR.BiodiverseCostaRica_ZH-CN5524154131_1920x1080.jpg";
+    bodyBgs[14] = "https://cn.bing.com/th?id=OHR.HiddenBeach_ZH-CN8410568637_1920x1080.jpg";
+    bodyBgs[15] = "https://cn.bing.com/th?id=OHR.ReefAwareness_ZH-CN8840949729_1920x1080.jpg";
+
+    var randomBgIndex = Math.floor(Math.random() * 16);
+    document.write('<style>.top-bg{background-image:url(' + bodyBgs[randomBgIndex] + ')}</style>');
+</script>                   
